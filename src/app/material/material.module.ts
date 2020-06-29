49eb11bdfbdb2176420da5850337de46553e8dda
Timothy Alcaide <timothy.alcaide@gmail.com>
2020-06-29 10:35:10 +0200

    add material module

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,33 @@
+import { LayoutModule } from '@angular/cdk/layout';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const modules = [
+  MatToolbarModule,
+  MatIconModule,
+  LayoutModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatListModule,
+  MatMenuModule,
+  MatIconModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSnackBarModule,
+];
+
+@NgModule({
+  imports: [...modules],
+  exports: [...modules],
+})
+export class MaterialModule {}
